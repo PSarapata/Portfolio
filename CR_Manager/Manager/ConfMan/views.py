5358f6cc7f153f7f040e316bbe88a15d23f5b0ac
PSarapata <pspsarapata@gmail.com>
2020-10-13 21:35:19 +0200

    Home view ('/' path) added

--- CR_Manager/Manager/ConfMan/views.py
+++ CR_Manager/Manager/ConfMan/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+import django.views.generic as dv
 
-# Create your views here.
+
+class HomeView(dv.View):
+    def get(self, request):
+        return render(request, 'ConfMan/html/index.html')
