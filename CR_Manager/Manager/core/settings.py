8ce148a908792fdd3ba62a9c1de664e49093b9f6
PSarapata <pspsarapata@gmail.com>
2020-10-14 22:04:34 +0200

    Homepage now fully functional! Onwards and upwards, Forward unto Dawn we shall go! See you tomorrow or the day after!

--- CR_Manager/Manager/core/settings.py
+++ CR_Manager/Manager/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#x0m@5p0bxtp0&o@oe*%i7i!)*2c6rejvmirg7j@993khu-l!_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
